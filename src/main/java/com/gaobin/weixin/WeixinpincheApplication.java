647905fb744fa0f53dce4666fa59290778aca047
gaobin <gaobin@workway.com.cn>
2017-02-20 17:13:59 +0800

    Frist

---
+++ src/main/java/com/gaobin/weixin/WeixinpincheApplication.java
@@ -0,0 +1,12 @@
+package com.gaobin.weixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeixinpincheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeixinpincheApplication.class, args);
+	}
+}
