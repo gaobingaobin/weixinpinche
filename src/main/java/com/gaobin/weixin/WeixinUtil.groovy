a549bcab9eaf32bec4adc84a2aacf0ae6043bd7a
gaobin <gaobin@workway.com.cn>
2017-02-20 17:36:45 +0800

    Frist

---
+++ src/main/java/com/gaobin/weixin/WeixinUtil.groovy
@@ -0,0 +1,13 @@
+package com.gaobin.weixin
+/**
+ * Created with IntelliJ IDEA.
+ * User: gaobin
+ * Date: 2017/2/20
+ * Time: 17:31
+ * To change this template use File | Settings | File Templates.
+ * Description: 获取相关信息的工具类
+ */
+class WeixinUtil {
+    private static final String APPID = "wx4a9c4f85ebf81d32";
+    private static final String SECRET = "6eaee22929aaee51df680a6b9dd0ab80";
+}
